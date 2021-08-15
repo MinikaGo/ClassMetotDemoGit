4b21ebc5dd6a0427d34641e983aa31e48e681d61
MinikaGo <usinanoglu@gmail.com>
2021-08-15 15:11:54 +0300

    Add files via upload

---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                ID = 1,
+                FirstName = "Furkan",
+                LastName = "Ogutcu",
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+            Console.WriteLine(customerManager.FullName(customer1));
+            customerManager.Add(customer1);
+            customerManager.Update(customer1);
+            customerManager.Delete(customer1);           
+        }
+    }
+}
